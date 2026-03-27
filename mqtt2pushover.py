edf570ed0a80b01c2cb610a07c097fe3b88989d2
Zach White <skullydazed@gmail.com>
2026-03-27 00:18:43 -0700

    Fix MQTT_TOPIC string concatenation

--- mqtt2pushover.py
+++ mqtt2pushover.py
@@ -12,7 +12,7 @@ MQTT_PORT = int(os.environ.get('MQTT_PORT', '1883'))
 MQTT_USER = os.environ.get('MQTT_USER', '')
 MQTT_PASS = os.environ.get('MQTT_PASS', '')
 MQTT_TOPIC_PREFIX = os.environ.get('MQTT_TOPIC', 'pushover')
-MQTT_TOPIC = MQTT_TOPIC_PREFIX + '/#')
+MQTT_TOPIC = MQTT_TOPIC_PREFIX + '/#'
 
 PUSHOVER_TOKEN = os.environ.get('PUSHOVER_TOKEN', '')
 PUSHOVER_USER = os.environ.get('PUSHOVER_USER', '')
